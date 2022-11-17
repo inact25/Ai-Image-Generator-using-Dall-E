aedbd00676acb1bfb14d8df0dd55cc25ed32e81e
Angga Pixa <anggas25@live.com>
2022-11-18 02:48:52 +0700

    - add 3D modes - add Painting modes - add Digital Art modes - redesign layout

--- src/App.js
+++ src/App.js
@@ -1,72 +1,119 @@
 import './App.css';
 import {generateAiImage, openai} from "./services/api";
-import {useState} from "react";
-import {Col, Input, Layout, Row} from "antd";
+import {useEffect, useState} from "react";
+import {Button, Col, Input, Layout, Radio, Row, Typography} from "antd";
+import Swal from "sweetalert2";
+import {Content} from "antd/es/layout/layout";
+import {IoLogoElectron} from "react-icons/io5";
 
 function App() {
     const [data, setData] = useState([])
     const [prompt, setPropmt] = useState('')
     const [isLoading, setIsLoading] = useState(false)
-    const {Search} = Input;
-    const {Header, Content, Footer} = Layout;
-    const getAiImage = (prompt) => {
+    const [modes, setModes] = useState('')
+
+    const handleModes = (key) => {
+
+        let map = {
+            "three-dimension": ", 3D",
+            "digital": ", Digital Art",
+            "painting": ", Outpainting",
+            "normal": "",
+        }
+        return map[key]
+
+    }
+
+    const getAiImage = async (prompt, modes) => {
         setIsLoading(true)
-        generateAiImage(prompt, "1024x1024")
+        let curentMode = await handleModes(modes)
+
+        generateAiImage(prompt, "1024x1024", curentMode)
             .then(e => {
                 setData(e.data.data)
                 setIsLoading(false)
             })
             .catch(e => {
-                alert(e)
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: e.message,
+                })
                 setIsLoading(false)
             })
     }
 
 
-    return (
-        <Layout className="App">
-            <Content style={{padding: 50, minHeight: "100vh"}}>
-                <div style={{marginTop: "10%"}}>
-                    <Search
-                        style={{maxWidth: 500, marginBottom: "2rem"}}
-                        size='large'
-                        placeholder="lets tell your story today"
-                        onChange={e => setPropmt(e.target.value)}
-                        onSearch={(e) => getAiImage(prompt)}
-                        loading={isLoading}
-                        enterButton
-                    />
-                    <div style={{marginBottom: "3rem"}}>
-                        <h2>{prompt}</h2>
-                    </div>
-                    {!isLoading ?
-                        <div>
-                            <Row gutter={[16, 16]} justify={"center"} align={"middle"}>
-                                {data?.map(item =>
-                                    <Col md={3}>
-                                        <img loading='lazy' style={{
-                                            width: "100%",
-                                            height: "auto",
-                                            borderRadius: "1rem",
-                                            objectFit: "contain"
-                                        }}
-                                             src={item.url} alt=""/>
-                                    </Col>
-                                )}
-                            < /Row>
 
-                        </div> : <img src="/loader.gif" width={100}  alt=""/>}
-                    <div style={{margin: "2rem"}}>
-                        <h2>Turn your story to unique images</h2>
-                    </div>
-                </div>
+    return (
+        <Layout className="App" style={{width: "100%"}}>
+            <Content>
+                <Row justify={"start"} align={"middle"} style={{minHeight: '100vh'}}>
+                    <Col lg={8} style={{
+                        background: '#212129',
+                        minHeight: "100vh",
+                        padding: "5rem",
+                        paddingTop: "10rem",
+                        color: 'whitesmoke',
+                    }}>
+                        <Typography style={{fontSize: 68, color: "whitesmoke"}}><IoLogoElectron/></Typography>
+                        <h1 style={{color: "white", marginBottom: "5rem"}}>Turn your story to unique images</h1>
+                        <Input.TextArea
+                            style={{marginBottom: "2rem", background: "white", borderRadius: 8}}
+                            size='large'
+                            placeholder="lets tell your story today"
+                            onChange={e => setPropmt(e.target.value)}
+                            value={prompt}
+                            rows={6}
+                            maxRows={6}
+                            autoSize={false}
+                            allowClear
+                            bordered={false}
+                        />
+                        <div>
+                            <p style={{fontWeight: 700}}>Modes</p>
+                        </div>
+                        <div>
+                            <Radio.Group size={"large"} onChange={e => setModes(e.target.value)} value={modes}>
+                                <Radio.Button value="normal">Normal</Radio.Button>
+                                <Radio.Button value="three-dimension">3D</Radio.Button>
+                                <Radio.Button value="painting">Painting</Radio.Button>
+                                <Radio.Button value="digital">Digital Art</Radio.Button>
+                            </Radio.Group>
+                        </div>
+                        <div style={{marginTop: "5rem"}}>
+                            <Button size={"large"} disabled={isLoading} onClick={() => getAiImage(prompt, modes)} block>Generate Image</Button>
+                        </div>
+                    </Col>
+                    <Col lg={16} style={{
+                        minHeight: "100vh",
+                        padding: "5rem",
+                        paddingTop: "10rem",
+                    }}>
+                        <div>
+                            {!isLoading ?
+                                <div>
+                                    <h2 style={{marginBottom: "3rem"}}>{prompt || "I dream to paint and then I paint my dream - Vincent Van Gogh"}</h2>
+                                    <Row gutter={[16, 16]} justify={"center"} align={"middle"}>
+                                        {data?.map(item =>
+                                            <Col md={6}>
+                                                <img loading='lazy' style={{
+                                                    width: "100%",
+                                                    height: "auto",
+                                                    borderRadius: "1rem",
+                                                    objectFit: "contain"
+                                                }}
+                                                     src={item.url} alt=""/>
+                                            </Col>
+                                        )}
+                                    < /Row>
+
+                                </div> : <img src="/loader.gif" width={100} alt=""/>}
+                        </div>
+                    </Col>
+                </Row>
+
             </Content>
-            <div style={{marginTop:"-5rem"}}>
-                <img width={38} height={38} style={{borderRadius:"100%", marginBottom:"1rem"}}
-                     src="https://media-exp1.licdn.com/dms/image/C560BAQFT9vMllW1AjA/company-logo_200_200/0/1616034348143?e=2147483647&v=beta&t=MyH5h7P6-GsEnjUWTv00qoDQqSl9hZQyGy9ahOji72U"
-                     alt=""/>
-                <div><a href="https://javapixa.com">javapixa creative studio</a></div>
-            </div>
         </Layout>
     );
 }
--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,18 +1,21 @@
 const {Configuration, OpenAIApi} = require("openai");
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
-const generateAiImage = async (prompt, size) => {
+const generateAiImage = async (prompt, size, mode) => {
     return await openai.createImage({
-        prompt: prompt,
+        prompt: prompt + mode,
         n: 8,
         size: size,
     });
 
 }
+
+
 export {
-    generateAiImage
+    generateAiImage,
+
 }
 
